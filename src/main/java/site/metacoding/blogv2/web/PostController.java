1b32d60711d55a2a1d6bd48ca962a76ca6d16ca1
jaewon2336 <xldzjqpf1588@naver.com>
2022-03-30 14:48:38 +0900

    15. 글쓰기 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -12,6 +12,13 @@ public class PostController {
 
     private final PostService postService;
 
+    // 페이지를 줘
+    // /s가 붙었으니까 자동으로 인터셉터가 인증 체크함
+    @GetMapping("/s/post/writeForm")
+    public String writeForm() {
+        return "post/writeForm";
+    }
+
     @GetMapping({ "/", "/post" })
     public String home() {
         return "post/list";
